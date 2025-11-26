5207fc976bc19425e1fc9863e0226a850956b568
Stephen Renwick <stephen.renwick@omicronenergy.com>
2025-11-26 08:16:39 +0100

    Deploy

---
+++ demo/AddPlugins.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-alert */
+export default class AddPlugins extends HTMLElement {
+  /* eslint-disable-next-line class-methods-use-this */
+  run() {
+    const editor = this.getRootNode().host;
+    const kind = window.confirm(
+      `Add a menu type plugin?
+If you choose 'Cancel', an editor type plugin will be added instead.`,
+    )
+      ? 'menu'
+      : 'editor';
+    const requireDoc = window.confirm(
+      'Does the plugin require a loaded document? (OK=yes, Cancel=no)',
+    );
+    const name =
+      window.prompt('Plugin name', 'My plugin') || 'Default plugin name';
+    const icon =
+      window.prompt('Plugin icon (material icon name)', 'extension') ||
+      'extension';
+    const src =
+      window.prompt(
+        'Plugin source URI',
+        `/demo/DemoPluginSrc.js?${Date.now()}`,
+      ) || 'data:text/javascript,';
+    const plugin = { name, src, icon, requireDoc };
+    if (
+      !window.confirm(
+        `Add ${kind} plugin ${JSON.stringify(plugin, null, ' ')}?`,
+      )
+    ) {
+      return;
+    }
+    editor.plugins = {
+      ...editor.plugins,
+      [kind]: [...(editor.plugins[kind] || []), plugin],
+    };
+    editor.requestUpdate('plugins');
+  }
+}
---
+++ locales/de.d.ts
@@ -0,0 +1,10 @@
+export declare const templates: {
+    s1b528387ae29637a: string;
+    s2ceb11be2290bb1b: string;
+    s60320a18282b2b33: string;
+    s690714fde574c188: string;
+    s6d8c02aee480af7a: string;
+    s737e3dbdf9c08cdf: string;
+    s7417792bbe720149: string;
+    s8f4be9f086eb530f: string;
+};
