2e4b4e6890f69d58d5395d8dd8b0138eb893f9da
Stephen Renwick <stephen.renwick@omicronenergy.com>
2025-11-26 08:19:30 +0100

    Updates

--- locales/de.js
+++ locales/de.js
@@ -1,22 +1,19 @@
 // Do not modify this file by hand!
-    // Re-generate this file by running lit-localize
+// Re-generate this file by running lit-localize
 
-    
-    
+/* eslint-disable no-irregular-whitespace */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-    /* eslint-disable no-irregular-whitespace */
-    /* eslint-disable @typescript-eslint/no-explicit-any */
-
-    const templates = {
-      's8f4be9f086eb530f': `Undo`,
-'s7417792bbe720149': `Redo`,
-'s6d8c02aee480af7a': `Menu`,
-'s690714fde574c188': `Filename`,
-'s1b528387ae29637a': `Extension`,
-'s737e3dbdf9c08cdf': `Close file`,
-'s2ceb11be2290bb1b': `Cancel`,
-'s60320a18282b2b33': `Rename`,
-    };
+const templates = {
+  s8f4be9f086eb530f: `Unfuck`,
+  s7417792bbe720149: `Redo`,
+  s6d8c02aee480af7a: `Menu`,
+  s690714fde574c188: `Filename`,
+  s1b528387ae29637a: `Extension`,
+  s737e3dbdf9c08cdf: `Close file`,
+  s2ceb11be2290bb1b: `Cancel`,
+  s60320a18282b2b33: `Rename`,
+};
 
 export { templates };
 //# sourceMappingURL=de.js.map
